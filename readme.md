ebc99b948efac557fa664273ce080ae0bd488881
Abdelkader youni <abdelyouni@gmail.com>
2020-12-29 00:34:06 +0100

    Init

---
+++ readme.md
@@ -0,0 +1,115 @@
+# JS TikTok Downloader
+Download TikTok Video or Music  & get all relative meta infos **using only JavaScript.**
+
+## Supported links :
+- www.tiktok.com/username/video/.....
+- m.tiktok.com/v/.....
+- vm.tiktok.com/.....
+
+## Example
+###### Basic usage :
+
+```html
+//Import the class
+<script src="tiktok.class.js"></script>
+
+<script>
+const TikTok = new TikGet('TIKTOK_VIDEO_URL');
+TikTok.get().then(function(){
+	//Log the video title
+	console.log(TikTok.datas.video.title);
+	
+	//Start download
+	TikTok.video_download();
+});
+</script>
+```
+#### Force Download
+
+###### Force Download Video :
+Start video download :
+```
+const TikTok = new TikGet('TIKTOK_VIDEO_URL');
+
+TikTok.get().then(function(){
+
+	TikTok.video_download();
+	
+});
+```
+###### Force Download Music :
+Start music download :
+
+```
+const TikTok = new TikGet('TIKTOK_VIDEO_URL');
+
+TikTok.get().then(function(){
+
+	TikTok.music_download();
+
+});
+```
+
+## Properties
+#### Video Infos
+| Name  | Property   |
+| ------------ | ------------ |
+| Video ID  | TikTok.datas.video.id  |
+| Video Title  | TikTok.datas.video.title  |
+|  Video Keywords | TikTok.datas.video.keywords  |
+| Video Description  | TikTok.datas.video.Description  |
+|  Video Height | TikTok.datas.video.height  |
+| Video Duration (in secondes)  | TikTok.datas.video.duration  |
+|  Video Resolution | TikTok.datas.video.sizeFormat  |
+| Video Cover  | TikTok.datas.video.cover  |
+|  Video Animated Cover | TikTok.datas.video.animatedCover  |
+| Video Likes Count  | TikTok.datas.video.likes  |
+|  Video Shares Count | TikTok.datas.video.shares  |
+| Video Comments Count  | TikTok.datas.video.comments  |
+|  Video Vues Count | TikTok.datas.video.vues  |
+|  Video Original Url | TikTok.datas.video.original_url  |
+|  Video Download Url | TikTok.datas.video.download_url  |
+
+#### Music Infos
+| Name  | Parametre  |
+| ------------ | ------------ |
+| Music ID  | TikTok.datas.music.id  |
+| music Title  | TikTok.datas.music.title  |
+|  Large size Cover | TikTok.datas.music.cover_large  |
+| Medium size Cover  | TikTok.datas.music.cover_medium  |
+|  Small size Cover | TikTok.datas.music.cover_small  |
+| Singer (Artist)  | TikTok.datas.music.artist  |
+|  Album name | TikTok.datas.music.album  |
+| Duration  | TikTok.datas.music.duration  |
+|  Stream URL | TikTok.datas.music.url  |
+| Download URL | TikTok.datas.music.download_url  |
+
+#### Author Infos
+| Name  | Parametre  |
+| ------------ | ------------ |
+| Author ID  | TikTok.datas.author.id  |
+| Unique ID  | TikTok.datas.author.uniqueId  |
+| Username  | TikTok.datas.author.username  |
+| Large size Avatar  | TikTok.datas.author.avatar_large  |
+| Medium size Avatar  | TikTok.datas.author.avatar_medium  |
+| Small size Avatar  | TikTok.datas.author.avatar_small  |
+| Signature  | TikTok.datas.author.signature  |
+| SignUp date  | TikTok.datas.author.createDate  |
+| Is the profile verified ?  | TikTok.datas.author.isVerified  |
+| Followers Count  | TikTok.datas.author.followers|
+| Followings Count  | TikTok.datas.author.followings  |
+| Likes recieved Count  | TikTok.datas.author.hearts  |
+| Likes given Count  | TikTok.datas.author.diggCount  |
+| Total Videos  | TikTok.datas.author.totalVideos  |
+## Php Version
+[TikGet - TikTok video downloader php class](https://github.com/abdelyouni/TikGet "TikGet - TikTok video downloader php class")
+## License
+(The MIT License)
+
+Copyright (c) 2020 Abdel Youni
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ tiktok.class.js
@@ -0,0 +1,125 @@
+/**
+ * JS tiktok downloader
+ * @version 1.0.0
+ * @author  Abdel Youni <abdelyouni@gmail.com>
+ * @see     https://github.com/abdelyouni/JS-TikTok-Downloader
+ */
+class TikGet {
+    constructor(url) {
+      this.url = url;
+      this.datas = null;
+    }
+    bypassCorsHeaders = "https://cors-tiktok.herokuapp.com/?u="; //Bypass CORS (Github : https://github.com/abdelyouni/cors-tiktok)
+
+    urlencode(str) {
+        str = (str + '').toString();
+        return encodeURIComponent(str)
+            .replace('!', '%21')
+            .replace('\'', '%27')
+            .replace('(', '%28')
+            .replace(')', '%29')
+            .replace('*', '%2A')
+            .replace('%20', '+');
+    }
+    force_download(url,title,format){
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url, true);
+        xhr.responseType = 'blob';
+        xhr.onload = function(e) {
+            var blob = xhr.response;
+            const fileName = title+'.'+format;
+            if (window.navigator && window.navigator.msSaveOrOpenBlob) { //IE
+                window.navigator.msSaveOrOpenBlob(blob, fileName);
+            } else { // for others
+                var url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = fileName;
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+            };
+        };
+        xhr.send();		 
+    }
+
+    get = async () => {
+        var patterns = ['<link data-react-helmet="true" rel="canonical" href="','"/>','>','</','id="__NEXT_DATA__"']
+        var tiktokUrl = this.bypassCorsHeaders + this.urlencode(this.url);
+        var resp =  await fetch(tiktokUrl).then(response => response.text()).then((data) => { return data; });
+        
+        if (resp.includes(patterns[0])) {
+            tiktokUrl = explode(this.patterns[1], explode(this.patterns[0], resp)[1])[0];
+            tiktokUrl = resp.split(patterns[0])[1].split(patterns[1])[0];
+            resp =  await fetch(tiktokUrl).then(response => response.text()).then((data) => { return data; });
+        }
+        
+        var json = JSON.parse(resp.split(patterns[4])[1].split(patterns[3])[0].split(patterns[2])[1]);
+
+        var video = {
+            id:json.props.pageProps.itemInfo.itemStruct.video.id,
+            height:json.props.pageProps.itemInfo.itemStruct.video.height,
+            duration:json.props.pageProps.itemInfo.itemStruct.video.duration,
+            sizeFormat:json.props.pageProps.itemInfo.itemStruct.video.ratio,
+            cover:json.props.pageProps.itemInfo.itemStruct.video.cover,
+            animatedCover:json.props.pageProps.itemInfo.itemStruct.video.dynamicCover,
+            likes:json.props.pageProps.itemInfo.itemStruct.stats.diggCount,
+            shares:json.props.pageProps.itemInfo.itemStruct.stats.shareCount,
+            comments:json.props.pageProps.itemInfo.itemStruct.stats.commentCount,
+            vues:json.props.pageProps.itemInfo.itemStruct.stats.playCount,
+            title:json.props.pageProps.metaParams.title,
+            keywords:json.props.pageProps.metaParams.keywords,
+            description:json.props.pageProps.metaParams.description,
+            original_url:json.props.pageProps.metaParams.canonicalHref,
+            download_url:json.props.pageProps.itemInfo.itemStruct.video.downloadAddr,
+        };
+
+        var music = {
+            id:json.props.pageProps.itemInfo.itemStruct.music.id,
+            title:json.props.pageProps.itemInfo.itemStruct.music.title,
+            cover_large:json.props.pageProps.itemInfo.itemStruct.music.coverLarge,
+            cover_medium:json.props.pageProps.itemInfo.itemStruct.music.coverMedium,
+            cover_small:json.props.pageProps.itemInfo.itemStruct.music.coverThumb,
+            artist:json.props.pageProps.itemInfo.itemStruct.music.authorName,
+            album:json.props.pageProps.itemInfo.itemStruct.music.album,
+            duration:json.props.pageProps.itemInfo.itemStruct.music.duration,
+            url:json.props.pageProps.itemInfo.itemStruct.music.playUrl,
+        };
+
+        var author = {
+            id:json.props.pageProps.itemInfo.itemStruct.author.id,
+            uniqueId:json.props.pageProps.itemInfo.itemStruct.author.uniqueId,
+            username:json.props.pageProps.itemInfo.itemStruct.author.nickname,
+            avatar_large:json.props.pageProps.itemInfo.itemStruct.author.avatarLarger,
+            avatar_medium:json.props.pageProps.itemInfo.itemStruct.author.avatarMedium,
+            avatar_small:json.props.pageProps.itemInfo.itemStruct.author.avatarThumb,
+            signature:json.props.pageProps.itemInfo.itemStruct.author.signature,
+            createDate:json.props.pageProps.itemInfo.itemStruct.author.createTime,
+            isVerified:json.props.pageProps.itemInfo.itemStruct.author.verified,
+            followers:json.props.pageProps.itemInfo.itemStruct.authorStats.followerCount,
+            followings:json.props.pageProps.itemInfo.itemStruct.authorStats.followingCount,
+            hearts:json.props.pageProps.itemInfo.itemStruct.authorStats.heart,
+            totalVideos:json.props.pageProps.itemInfo.itemStruct.authorStats.videoCount,
+            diggCount:json.props.pageProps.itemInfo.itemStruct.authorStats.diggCount,
+        };
+
+        this.datas = {video:video,music:music,author:author};
+        
+
+    }
+    download_music = async () => {
+        if(this.datas == null){
+            await this.get();
+        }
+        this.force_download(this.bypassCorsHeaders + this.urlencode(this.datas.music.url) + "&d=1",this.datas.music.id,'mp3');
+    }
+    download_video = async () => {
+        if(this.datas == null){
+            await this.get();
+        }
+        this.force_download(this.bypassCorsHeaders + this.urlencode(this.datas.video.download_url) + "&d=1",this.datas.video.id,'mp4');
+    }
+
+
+}
